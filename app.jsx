b6da56ea4c44ad604fe00f52fe5758be275babe4
Shayban Nasif <45189438+Shayban-Nasif@users.noreply.github.com>
2025-12-16 14:42:26 +0900

    app.jsx

---
+++ app.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Asdiqa</h1>
+      <p>Welcome to Asdiqa website</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
